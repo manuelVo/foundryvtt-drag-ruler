b29a687fff3070479f8da3be687a8e3c9121abff
Grygon <647846+Grygon@users.noreply.github.com>
2021-02-11 11:36:46 -0600

    Add Lancer to systems.js (#21)

--- src/systems.js
+++ src/systems.js
@@ -3,6 +3,8 @@ export function getDefaultSpeedAttribute() {
 	switch (game.system.id) {
 		case "dnd5e":
 			return "actor.data.data.attributes.movement.walk"
+		case "lancer":
+			return "actor.data.data.mech.speed"
 		case "pf1":
 			return "actor.data.data.attributes.speed.land.total"
 	}
@@ -12,6 +14,7 @@ export function getDefaultSpeedAttribute() {
 export function getDefaultDashMultiplier() {
 	switch (game.system.id) {
 		case "dnd5e":
+		case "lancer":
 		case "pf1":
 			return 2
 	}
