69087e64a55d9ec59036f245bcf00e4d23fb4774
Grygon <647846+Grygon@users.noreply.github.com>
2021-02-11 10:53:03 -0600

    Update systems.js

--- src/systems.js
+++ src/systems.js
@@ -5,6 +5,8 @@ export function getDefaultSpeedAttribute() {
 			return "actor.data.data.attributes.movement.walk"
 		case "pf1":
 			return "actor.data.data.attributes.speed.land.total"
+		case "lancer":
+			return "actor.data.data.mech.speed"
 	}
 	return ""
 }
@@ -13,6 +15,7 @@ export function getDefaultDashMultiplier() {
 	switch (game.system.id) {
 		case "dnd5e":
 		case "pf1":
+		case "lancer":
 			return 2
 	}
 	return 0
