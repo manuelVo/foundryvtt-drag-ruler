a09ebc443997867f11242ab66feebfda1808d27a
Jacob Dorman <JacobOnFire@gmail.com>
2021-03-15 17:24:45 -0400

    added cyberpunk-red-core default settings and getDefaultSpeedMultiplier helper method

--- src/systems.js
+++ src/systems.js
@@ -7,16 +7,27 @@ export function getDefaultSpeedAttribute() {
 			return "actor.data.data.mech.speed"
 		case "pf1":
 			return "actor.data.data.attributes.speed.land.total"
+		case "cyberpunk-red-core":
+			return "actor.data.data.stats.move.value"
 	}
 	return ""
 }
 
 export function getDefaultDashMultiplier() {
 	switch (game.system.id) {
+		case "cyberpunk-red-core":
 		case "dnd5e":
 		case "lancer":
 		case "pf1":
 			return 2
 	}
 	return 0
+}
+
+export function getDefaultSpeedMultiplier() {
+	switch (game.system.id) {
+		case "cyberpunk-red-core":
+			return 2
+	}
+	return 1
 }
