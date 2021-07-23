7f6c7a0525b25a400a66a0032cd554d246ac7d48
MAClavell <michaelclavell11@gmail.com>
2021-07-22 22:39:58 -0400

    Remove an unused case from some testing I did

--- src/compatibility.js
+++ src/compatibility.js
@@ -35,15 +35,10 @@ export function measureDistances(segments, entity, shape, options={}) {
 		return distances.concat(terrainRuler.measureDistances(newSegments, opts));
 	}
 	else {
-		// If another module wants to enable grid measurements but disabled grid highlighting,
-		// manually set the *duplicate* option's gridSpaces value to true for Foundry
+		// If another module wants to enable grid measurements but disable grid highlighting,
+		// manually set the *duplicate* option's gridSpaces value to true for the Foundry logic to work properly
 		if(!opts.ignoreGrid && !opts.gridSpaces) {
 			opts.gridSpaces = true;
-		}
-		// If another module wants to disable grid measurements but enabled grid highlighting,
-		// manually set the *duplicate* option's gridSpaces value to false for Foundry
-		else if(opts.ignoreGrid && opts.gridSpaces) {
-			opts.gridSpaces = false;
 		}
 		return canvas.grid.measureDistances(segments, opts);
 	}
