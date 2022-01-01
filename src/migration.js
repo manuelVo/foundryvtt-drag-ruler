b06169f149f62d0e360595b56fc245415eed496c
Manuel Vögele <develop@manuel-voegele.de>
2022-01-01 17:25:03 +0100

    Remove migration from unnamed version

--- src/migration.js
+++ src/migration.js
@@ -8,19 +8,6 @@ export function performMigrations() {
 
 	let dataVersion = game.settings.get(settingsKey, "dataVersion")
 	if (dataVersion === "fresh install") {
-		// Migration from unnamed version. TODO Remove this in a future version
-		let speedAttribute = game.settings.storage.get("world").get(`${settingsKey}.speedAttribute`)
-		if (speedAttribute)
-			speedAttribute = speedAttribute.slice(1, speedAttribute.length - 1)
-		const speedAttributeDefault = game.settings.get(settingsKey, "speedProviders.native.setting.speedAttribute")
-		if (speedAttribute !== speedAttributeDefault)
-			game.settings.set(settingsKey, "speedProviders.native.setting.speedAttribute", speedAttribute)
-
-		let dashMultiplier = game.settings.storage.get("world").get(`${settingsKey}.dashMultiplier`)
-		const dashMultiplierDefault = game.settings.get(settingsKey, "speedProviders.native.setting.dashMultiplier")
-		if (dashMultiplier !== dashMultiplierDefault)
-			game.settings.set(settingsKey, "speedProviders.native.setting.dashMultiplier", dashMultiplier)
-		// End of unnamed version migration code
 		game.settings.set(settingsKey, "dataVersion", currentDataVersion)
 		return
 	}
