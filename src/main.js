58946096a6b566a5494177d0d8ba669aa8722203
Manuel Vögele <develop@manuel-voegele.de>
2022-02-12 18:25:50 +0100

    Remove debugging output

--- src/main.js
+++ src/main.js
@@ -97,7 +97,6 @@ async function tokenLayerUndoHistory(wrapped) {
 
 function onEntityLeftDragStart(wrapped, event) {
 	wrapped(event);
-	console.warn("start", Date.now());
 	const isToken = this instanceof Token;
 	const ruler = canvas.controls.ruler
 	ruler.draggedEntity = this;
@@ -128,7 +127,6 @@ function onEntityLeftDragMove(wrapped, event) {
 
 function onEntityDragLeftDrop(event) {
 	const ruler = canvas.controls.ruler
-	console.warn("stop", ruler._state, Date.now());
 	if (!ruler.isDragRuler) {
 		ruler.draggedEntity = undefined;
 		return false
