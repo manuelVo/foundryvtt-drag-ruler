61c48dff5e573fe8be448bf2e57e8d811a274a03
Manuel Vögele <develop@manuel-voegele.de>
2022-02-15 17:54:19 +0100

    Force tokens to keep some distance from the outer walls as well

--- rust/src/pathfinder.rs
+++ rust/src/pathfinder.rs
@@ -188,14 +188,17 @@ impl Pathfinder {
 				if angle_diff <= PI {
 					continue;
 				}
-				if angle_diff > 1.5 * PI {
+				{
 					let angle_between = angle_diff / 2.0 + angle1;
-					nodes.push(calc_pathfinding_node(
+					let pathfinding_node = calc_pathfinding_node(
 						point,
 						angle_between,
 						distance_from_walls,
 						&mut line_segments,
-					));
+					);
+					if angle_diff > 1.5 * PI {
+						nodes.push(pathfinding_node);
+					}
 				}
 				nodes.push(calc_pathfinding_node(
 					point,
