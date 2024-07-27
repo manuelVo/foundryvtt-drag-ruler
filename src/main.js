0e2ab35db71b3ca00251eeea830de2da8c4d6629
n0q <n0q@users.noreply.github.com>
2024-07-27 10:22:35 -0700

    fix hex calculations in v12

--- src/main.js
+++ src/main.js
@@ -137,10 +137,11 @@ function onEntityLeftDragStart(wrapped, event) {
 	const ruler = canvas.controls.ruler;
 	ruler.draggedEntity = this;
 	const entityCenter = getEntityCenter(this);
-	ruler.rulerOffset = {
-		x: entityCenter.x - event.interactionData.origin.x,
-		y: entityCenter.y - event.interactionData.origin.y,
-	};
+	const isV11 = game.release.generation === 11;
+    ruler.rulerOffset = {
+    	x: isV11 ? entityCenter.x - event.interactionData.origin.x : 0,
+      	y: isV11 ? entityCenter.y - event.interactionData.origin.y : 0,
+    };
 	if (game.settings.get(settingsKey, "autoStartMeasurement")) {
 		let options = {};
 		setSnapParameterOnOptions(ruler, options);
--- src/util.js
+++ src/util.js
@@ -157,7 +157,8 @@ export function getAreaFromPositionAndShape(position, shape) {
 		let y = position.y + space.y;
 		if (isCanvasHex()) {
 			let shiftedRow;
-			if (canvas.grid.grid.options.even) shiftedRow = 1;
+			// v12 ?? v11
+			if (canvas.grid?.even ?? canvas.grid.grid.options.even) shiftedRow = 1;
 			else shiftedRow = 0;
 			if (canvas.grid.grid.columnar) {
 				if (space.x % 2 !== 0 && position.x % 2 !== shiftedRow) {
